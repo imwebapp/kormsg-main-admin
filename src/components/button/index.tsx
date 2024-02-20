73061937811fabc92ae84582e6f11fbe52af8e2d
Hoang Nguyen <nviethoang98@gmail.com>
2024-02-20 10:59:55 +0700

    component button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode, PropsWithChildren } from "react";
+import { Button, ButtonProps, Space } from "antd";
+import { useTranslation } from "react-i18next";
+import { classNames } from "../../utils/common";
+import "./styles.css";
+interface CustomButtonProps extends ButtonProps {
+  children?: ReactNode;
+  locale?: boolean;
+  icon?: ReactNode;
+  bold?: boolean;
+  medium?: boolean;
+  className?: string; // for tailwindcss
+}
+
+export default function CustomButton(
+  props: PropsWithChildren<CustomButtonProps>
+) {
+  const { locale, children, bold, icon, medium, className, ...newProps } =
+    props;
+  const { t } = useTranslation();
+
+  return (
+    <Button
+      className={classNames(
+        bold ? "font-bold" : "",
+        medium ? "font-medium" : "",
+        className || ""
+      )}
+      {...newProps}
+    >
+      <Space>
+        {icon && <span>{icon}</span>}
+        {locale ? t(children as string) : children}
+      </Space>
+    </Button>
+  );
+}
