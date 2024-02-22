877dacbbd421dee29c99890f1c22fd896ec14e42
tiendatnguyenit <tiendatnguyenit@gmail.com>
2024-02-22 13:48:07 +0700

    update base modal component

--- src/components/modal/BaseModal.tsx
+++ src/components/modal/BaseModal.tsx
@@ -12,10 +12,12 @@ interface BaseModalProps {
     children: ReactNode;
     styleTitle?: string;
     disableSubmitBtn?: boolean;
+    nameCancel?: string;
+    nameConfirm?: string;
 }
 
 export const BaseModal = (props: BaseModalProps) => {
-    const { isOpen, onSubmit, onClose, title, children, disableSubmitBtn, styleTitle } = props;
+    const { isOpen, onSubmit, onClose, title, children, disableSubmitBtn, nameCancel, nameConfirm, styleTitle } = props;
     const [isShown, setIsShown] = useState<boolean>(isOpen);
 
     const closeModal = () => {
@@ -41,26 +43,26 @@ export const BaseModal = (props: BaseModalProps) => {
         <>
             {isShown && (
                 <div className="fixed inset-0 z-10">
-                    <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+                    <div className="flex items-center justify-center min-h-screen px-4 pt-4 text-center sm:block sm:p-0">
                         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
                             <div className="absolute inset-0 bg-black opacity-50" onClick={handleOutsideClick}></div>
                         </div>
                         <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
                         <div className="inline-block text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                            <div className="flex px-6 py-4 bg-white rounded-lg">
-                                <BaseText bold locale size={20} className={classNames("w-full font-bold text-left text-darkNight900", styleTitle)}>{title}</BaseText>
+                            <div className="flex items-center justify-between px-6 py-4 bg-white rounded-lg">
+                                <BaseText bold locale size={20} className={classNames("font-bold text-left text-darkNight900", styleTitle)}>{title}</BaseText>
                                 <CloseOutlined onClick={closeModal} className="text-2xl text-gray-500 cursor-pointer" />
                             </div>
-                            <div className="px-6 py-4 overflow-auto bg-white " style={{ maxHeight: '70vh' }}>
+                            <div className="max-h-[72vh] px-6 py-4 overflow-auto">
                                 {children}
                             </div>
                             <div className="flex gap-4 px-6 py-4 border-t border-darkNight100 sm:px-6">
                                 <CustomButton
                                     onClick={closeModal}
                                     locale
-                                    className="w-full "
+                                    className="w-full"
                                 >
-                                    Cancel
+                                    {nameCancel || 'Cancel'}
                                 </CustomButton>
                                 <CustomButton
                                     onClick={submitModal}
@@ -69,7 +71,7 @@ export const BaseModal = (props: BaseModalProps) => {
                                     className="w-full"
                                     disabled={disableSubmitBtn}
                                 >
-                                    Confirm
+                                    {nameConfirm || 'Confirm'}
                                 </CustomButton>
                             </div>
                         </div>
