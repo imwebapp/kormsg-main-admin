7f4ca2921b21b1a5f9bf7b387262d0f9a4b8726b
Hoang Nguyen Viet <nviethoang98@gmail.com>
2024-03-19 03:05:20 +0700

    get info bar chart

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -10,33 +10,9 @@ import {
   DashboardSearchTermTable,
   DashboardVisitTable,
 } from "../../components";
-import axios from "axios";
 import { analyticsApi } from "../../apis/analyticsApi";
 
 const Dashboard = () => {
-  useEffect(() => {
-    const getInfoAnalytics = async () => {
-      let result = await analyticsApi.getInfo({
-        property: "properties/244725891",
-        dateRanges: [
-          {
-            startDate: "7daysAgo",
-            endDate: "today",
-          },
-        ],
-        dimensions: [{ name: "date" }],
-        metrics: [
-          { name: "activeUsers" },
-          { name: "screenPageViews" },
-          { name: "sessions" },
-        ],
-      });
-      console.log("result", result.data);
-    };
-    getInfoAnalytics();
-    return () => {};
-  }, []);
-
   return (
     <div className="p-6">
       <DashboardStatistic />
