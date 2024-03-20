9e50ea956dd8c5a829544a72957c20b3e3a62bca
tiendatnguyenit <tiendatnguyenit@gmail.com>
2024-03-20 11:19:20 +0700

    integrate user manage

---
+++ src/apis/employeeApi .ts
@@ -0,0 +1,8 @@
+import axiosClient from './axiosClient'
+import {EMPLOYEE} from './urlConfig'
+
+export const employeeApi = {
+  getList: (params?: any) => {
+    return axiosClient.get(`${EMPLOYEE}`, {params})
+  },
+}
