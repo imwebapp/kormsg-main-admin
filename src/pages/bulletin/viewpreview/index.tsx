7945fe4d8108cd782c715caa5b3e4013a1adc146
luongduy2798 <nhatduy2727@gmail.com>
2024-05-28 14:36:26 +0700

    fixbug

--- src/pages/bulletin/viewpreview/index.tsx
+++ src/pages/bulletin/viewpreview/index.tsx
@@ -24,7 +24,7 @@ export default function BulletinPreview() {
         boardSelected.route ?? "",
         boardSelected.thema_id
       );
-      setUrl(BASE_URL + url);
+      setUrl(URL_SEARCH_SITE + url);
     }
   }, [boardSelected]);
 
