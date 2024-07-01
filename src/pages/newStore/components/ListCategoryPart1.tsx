19375ddbe4ecf1a29afbad592060edb6d72fcb7b
luongduy2798 <nhatduy2727@gmail.com>
2024-07-01 21:25:51 +0700

    fixbug

--- src/pages/newStore/components/ListCategoryPart1.tsx
+++ src/pages/newStore/components/ListCategoryPart1.tsx
@@ -4,25 +4,40 @@ import { classNames } from "../../../utils/common";
 import { useTranslation } from "react-i18next";
 
 interface IListCategoryPart {
-    title?: string;
-    value: string;
-    placeholder?: string;
-    onClick: () => void;
-    isLocale?: boolean;
+  title?: string;
+  subTitle?: any;
+  value: string;
+  placeholder?: string;
+  onClick: () => void;
+  isLocale?: boolean;
 }
 
 export const ListCategoryPart1 = (props: IListCategoryPart) => {
-    const { title, value, placeholder, onClick, isLocale } = props;
-    const { t } = useTranslation();
-    return (
-        <div className="flex flex-col gap-2">
-            {title && <BaseText locale size={16} bold>
-                {title}
-            </BaseText>}
-            <div className="flex justify-between p-3 rounded-lg bg-darkNight50" onClick={onClick}>
-                <BaseText size={16} className={classNames(value ? "" : 'text-darkNight300')} locale={isLocale}>{value || placeholder || ""}</BaseText>
-                <DownOutlined />
-            </div>
+  const { title, value, placeholder, onClick, isLocale } = props;
+  const { t } = useTranslation();
+  return (
+    <div className="flex flex-col gap-2">
+      {title && (
+        <div className="flex flex-row justify-between items-center">
+          <BaseText locale size={16} bold>
+            {title}
+          </BaseText>
+          {!!props.subTitle && props.subTitle}
         </div>
-    )
+      )}
+      <div
+        className="flex justify-between p-3 rounded-lg bg-darkNight50"
+        onClick={onClick}
+      >
+        <BaseText
+          size={16}
+          className={classNames(value ? "" : "text-darkNight300")}
+          locale={isLocale}
+        >
+          {value || placeholder || ""}
+        </BaseText>
+        <DownOutlined />
+      </div>
+    </div>
+  );
 };
